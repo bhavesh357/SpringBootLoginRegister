4e8a0f587b3606ee0d1486116a95c6436caf8901
bhavesh357 <bkadam357@gmail.com>
2020-06-25 15:07:42 +0530

    ADDED:feature to check the redirect to login oage

--- src/main/java/com/bl/loginregister/controller/UserController.java
+++ src/main/java/com/bl/loginregister/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class LoginController {
+public class UserController {
 
     @RequestMapping("/login")
     public String printLogin(){
--- src/test/java/com/bl/loginregister/LoginRegisterApplicationTests.java
+++ src/test/java/com/bl/loginregister/LoginRegisterApplicationTests.java
@@ -1,13 +1,31 @@
 package com.bl.loginregister;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class LoginRegisterApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    public void GivenAddress_ShouldReturnPage() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/login").accept(MediaType.ALL);
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        Assert.assertEquals("/WEB-INF/login.jsp",mvcResult.getResponse().getForwardedUrl());
     }
 
 }
